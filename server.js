1aef9c8b9098623381ccedbc263ecc5534d891b7
KhensaDPJ <59077006+KhensaDPJ@users.noreply.github.com>
2024-09-09 09:42:28 +0700

    update Hello world to project

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app =express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 app.use('/api', router);
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`)
