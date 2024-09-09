437c76cc8bec0e8bd1e4b1c9cc43c444da0b29bd
KhensaDPJ <59077006+KhensaDPJ@users.noreply.github.com>
2024-09-09 10:32:18 +0700

    remove token for list all product

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 import productController from "../controllers/product.controller.js";
 const router = express.Router();
 
-router.get('/product/list', authMiddleware.authenticateJWT,productController.productList);
+router.get('/product/list',productController.productList);
 router.post('/product/create', authMiddleware.authenticateJWT,productController.addProduct);
 router.patch('/product/update', authMiddleware.authenticateJWT,productController.updateProduct);
 router.delete('/product/delete/:id', authMiddleware.authenticateJWT,productController.deleteProduct);
